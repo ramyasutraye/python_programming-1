20ca5b3f6247acc4763b6c9bffcbd1ee5cc980c2
sureshkeerthi <36121201+sureshkeerthi@users.noreply.github.com>
2018-02-04 15:11:06 +0530

    Add files via upload

---
+++ beginner/mathoper.py
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+a=input(float())
+b=input(float())
+c=input(float())
+d=input(float())
+e=input(float())
+f=input(float())
+g=input(float())
+h=input(float())
+i=input(float())
+j=input(float())
+print(a*b*c*d*e*f*g*h*i*j)
+print(a-b-c-d-e-f-g-h-i-j)
+print(a%b%c%d%e%f%g%h%i%j)
+print(a+b+c+d+e+f+g+h+i+j)
+
