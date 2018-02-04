aa775d885c262ad5170f454ab4316452ac089b63
sureshkeerthi <36121201+sureshkeerthi@users.noreply.github.com>
2018-02-04 12:19:39 +0530

    Add files via upload

--- beginner/add.py
+++ beginner/add.py
@@ -1,5 +1,28 @@
-num1=input
-num2=input()
-sum=num1+num2
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 
-print(sum)
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=input()
+b=input()
+c=input()
+d=input()
+e=input()
+f=input()
+g=input()
+h=input()
+i=input()
+j=input()
+print("The answer is")
+print(a+b+c+d+e+f+g+h+i+j)
